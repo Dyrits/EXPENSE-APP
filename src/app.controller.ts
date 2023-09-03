9c834258b3bb8de3dd6a57167f9a88be03c61974
Dylan J. Gerrits <dyrits@outlook.com>
2023-09-03 18:52:11 +0200

    Refactor AppController and AppService for IncomeReport feature
    The AppController and AppService have been refactored to handle a new feature for an income report. The base route in AppController was changed from generic to "/report/incomes" specific. Simultaneously, the 'getHello()' function has been replaced by a new 'getIncomesReport()' function, which currently returns an empty array as placeholder. These changes set the preliminary groundwork for the new IncomeReport feature.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
 
-@Controller()
+@Controller("/report/incomes")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getIncomesReport() {
+    return this.appService.getIncomesReport();
   }
 }
